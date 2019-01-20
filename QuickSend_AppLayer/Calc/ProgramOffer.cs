23a8d63c3904b5892825fc8f75b22e2e0b400d25
MinYenYimYinYen <lukeholker@gmlawncare.onmicrosoft.com>
2019-01-20 16:21:07 -0600

    Added AppLayer project

---
+++ QuickSend_AppLayer/Calc/ProgramOffer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace QuickSend.Models
+{
+	public class ProgramOffer
+	{
+		public double PricePerApp { get; set; }
+		public double PricePerAppPPY => Math.Round(PricePerApp * (1 - PrePayPerc),2);
+		public int FreeAppCount { get; set; }
+
+		public int AppCount { get; set; }
+		public int PaidAppCount => AppCount - FreeAppCount;
+
+		public double GrossPrice => PaidAppCount * PricePerApp;
+		public double GrossPricePPY => GrossPrice * (1 - PrePayPerc);
+		public double PrePaySavings => PaidAppCount * (PricePerApp - PricePerAppPPY);
+
+		public double PrePayPerc { get; set; }		
+
+		public double TaxPercent { get; set; }
+		public double TaxDollars
+		{
+			get
+			{
+				double tax = 0;
+				for(int i = 0; i < PaidAppCount; i++)
+				{
+					tax += Math.Round(TaxPercent * PricePerApp, 2);
+				}
+				return tax;
+			}
+		}
+		public double TaxDollarsPPY
+		{
+			get
+			{
+				double tax = 0;
+				for (int i = 0; i < PaidAppCount; i++)
+				{
+					tax += Math.Round(TaxPercent * PricePerAppPPY, 2);
+				}
+				return tax;
+			}
+		}
+
+		public double Total => GrossPrice + TaxDollars;
+		public double TotalPPY => GrossPricePPY - PrePaySavings + TaxDollarsPPY;
+
+
+	}
+}
