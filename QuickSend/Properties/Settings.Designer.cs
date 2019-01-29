3745a5482a94342f9c736948a2081c8e06ec952b
MinYenYimYinYen <lukeholker@gmlawncare.onmicrosoft.com>
2019-01-28 20:27:21 -0600

    Added Icon

--- QuickSend/Properties/Settings.Designer.cs
+++ QuickSend/Properties/Settings.Designer.cs
@@ -58,5 +58,17 @@ namespace QuickSend.Properties {
                 this["AgroSheetFolder"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string Setting {
+            get {
+                return ((string)(this["Setting"]));
+            }
+            set {
+                this["Setting"] = value;
+            }
+        }
     }
 }
